47e7c6abc6c09a6d4e964fad9ebaa7459cdeebac
Andreas Grau <a.grau@tarent.de>
2013-09-27 10:12:39 +0200

    Extended test with exception assertion.

--- src/test/groovy/org/osiam/resources/scim/BuildOrderTest.groovy
+++ src/test/groovy/org/osiam/resources/scim/BuildOrderTest.groovy
@@ -23,6 +23,7 @@
 
 package org.osiam.resources.scim
 
+import org.codehaus.groovy.runtime.typehandling.GroovyCastException
 import org.osiam.resources.type.EmailType;
 import org.osiam.resources.type.ImsType;
 import org.osiam.resources.type.PhoneNumberType;
@@ -32,87 +33,90 @@ import spock.lang.Specification
 
 class BuildOrderTest extends Specification {
 
-	def "address should be able to build operation first"() {
-		given:
-		Address builder = new Address.Builder()
-				.setOperation("op")
-				.setCountry("country")
-				.build()
-	}
-	
-    def "email should be able to build bottom to top"() {
-    given:
-    Email builder = new Email.Builder()
-            .setOperation("op")
-            .setType(EmailType.HOME)
-			.build()
+    def "address should be able to build operation first"() {
+        when: "an address object is built with operation first"
+        Address builder = new Address.Builder()
+                .setOperation("op")
+                .setCountry("country")
+                .build()
+
+        then: "the return should be an address an throw no cast exception"
+        notThrown(GroovyCastException)
     }
-	
-	def "entitlement should be able to build bottom to top"() {
-		given:
-		Entitlement builder = new Entitlement.Builder()
-				.setOperation("op")
-				.setType("type")
-				.build()
-	}
-	
-	def "ims should be able to build bottom to top"() {
-		given:
-		Ims builder = new Ims.Builder()
-				.setOperation("op")
-				.setType(ImsType.AIM)
-				.build()
-	}
-	
-	def "phoneNumber should be able to build bottom to top"() {
-		given:
-		PhoneNumber builder = new PhoneNumber.Builder()
-				.setOperation("op")
-				.setType(PhoneNumberType.HOME)
-				.build()
-	}
-	
-	def "photo should be able to build bottom to top"() {
-		given:
-		Photo builder = new Photo.Builder()
-				.setOperation("op")
-				.setType(PhotoType.PHOTO)
-				.build()
-	}
-	
-	def "role should be able to build bottom to top"() {
-		given:
-		Role builder = new Role.Builder()
-				.setOperation("op")
-				.setType("type")
-				.build()
-	}
-	
-	def "x509Certificate should be able to build bottom to top"() {
-		given:
-		X509Certificate builder = new X509Certificate.Builder()
-				.setOperation("op")
-				.setType("type")
-				.build()
-	}
-	
-	def "user should be able to build bottom to top"() {
-		given:
-		User builder = new User.Builder()
-				.setId("")
-				.setExternalId("")
-				.setNickName("")
-				.build()
-	}
-	
-	def "group should be able to build bottom to top"() {
-		given:
-		Group builder = new Group.Builder()
-				.setId("")
-				.setExternalId("")
-				.setMembers(null)
-				.build()
-	}
+
+    def "email should be able to build bottom to top"() {
+        given:
+        Email builder = new Email.Builder()
+                .setOperation("op")
+                .setType(EmailType.HOME)
+                .build()
+    }
+
+    def "entitlement should be able to build bottom to top"() {
+        given:
+        Entitlement builder = new Entitlement.Builder()
+                .setOperation("op")
+                .setType("type")
+                .build()
+    }
+
+    def "ims should be able to build bottom to top"() {
+        given:
+        Ims builder = new Ims.Builder()
+                .setOperation("op")
+                .setType(ImsType.AIM)
+                .build()
+    }
+
+    def "phoneNumber should be able to build bottom to top"() {
+        given:
+        PhoneNumber builder = new PhoneNumber.Builder()
+                .setOperation("op")
+                .setType(PhoneNumberType.HOME)
+                .build()
+    }
+
+    def "photo should be able to build bottom to top"() {
+        given:
+        Photo builder = new Photo.Builder()
+                .setOperation("op")
+                .setType(PhotoType.PHOTO)
+                .build()
+    }
+
+    def "role should be able to build bottom to top"() {
+        given:
+        Role builder = new Role.Builder()
+                .setOperation("op")
+                .setType("type")
+                .build()
+    }
+
+    def "x509Certificate should be able to build bottom to top"() {
+        given:
+        X509Certificate builder = new X509Certificate.Builder()
+                .setOperation("op")
+                .setType("type")
+                .build()
+    }
+
+    def "user should be able to build bottom to top"() {
+        given:
+        User builder = new User.Builder()
+                .setId("")
+                .setExternalId("")
+                .setNickName("")
+                .build()
+    }
+
+    def "group should be able to build bottom to top"() {
+        given:
+        Group builder = new Group.Builder()
+                .setId("")
+                .setExternalId("")
+                .setMembers(null)
+                .build()
+    }
 
 
 }
