3ef00ea055d6828e24ea7862b5230522964cc822
David Möbius <d.moebius@tarent.de>
2013-09-27 14:53:54 +0200

    tests refactored to be more readebly

--- src/test/groovy/org/osiam/resources/scim/BuildOrderTest.groovy
+++ src/test/groovy/org/osiam/resources/scim/BuildOrderTest.groovy
@@ -49,77 +49,140 @@ class BuildOrderTest extends Specification {
     }
 
     def "email should be able to build bottom to top"() {
-        given:
+        when: "an Email object is built with operation first"
         Email builder = new Email.Builder()
                 .setOperation("op")
                 .setType(EmailType.HOME)
                 .build()
+				
+        then: "the return should be an Email so no cast exception should be thrown"
+        notThrown(GroovyCastException)
+
+        and: "all values should be set correctly"
+        builder.operation == "op"
+        builder.type == EmailType.HOME
     }
 
     def "entitlement should be able to build bottom to top"() {
-        given:
+        when: "an Entitlement object is built with operation first"
         Entitlement builder = new Entitlement.Builder()
                 .setOperation("op")
                 .setType("type")
                 .build()
+				
+		then: "the return should be an Entitlement so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.operation == "op"
+		builder.type == "type"
     }
 
     def "ims should be able to build bottom to top"() {
-        given:
+        when: "an Ims object is built with operation first"
         Ims builder = new Ims.Builder()
                 .setOperation("op")
                 .setType(ImsType.AIM)
                 .build()
+				
+		then: "the return should be an Ims so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.operation == "op"
+		builder.type == ImsType.AIM
     }
 
     def "phoneNumber should be able to build bottom to top"() {
-        given:
+        when: "an PhoneNumber object is built with operation first"
         PhoneNumber builder = new PhoneNumber.Builder()
                 .setOperation("op")
                 .setType(PhoneNumberType.HOME)
                 .build()
+				
+		then: "the return should be an PhoneNumber so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.operation == "op"
+		builder.type == PhoneNumberType.HOME
     }
 
     def "photo should be able to build bottom to top"() {
-        given:
+        when: "an Photo object is built with operation first"
         Photo builder = new Photo.Builder()
                 .setOperation("op")
                 .setType(PhotoType.PHOTO)
                 .build()
+				
+		then: "the return should be an Photo so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.operation == "op"
+		builder.type == PhotoType.PHOTO
     }
 
     def "role should be able to build bottom to top"() {
-        given:
+        when: "an Role object is built with operation first"
         Role builder = new Role.Builder()
                 .setOperation("op")
                 .setType("type")
                 .build()
+				
+		then: "the return should be an Role so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.operation == "op"
+		builder.type == "type"
     }
 
     def "x509Certificate should be able to build bottom to top"() {
-        given:
+        when: "an X509Certificate object is built with operation first"
         X509Certificate builder = new X509Certificate.Builder()
                 .setOperation("op")
                 .setType("type")
                 .build()
+				
+		then: "the return should be an X509Certificate so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.operation == "op"
+		builder.type == "type"
     }
 
     def "user should be able to build bottom to top"() {
-        given:
+        when: "an User object is built with operation first"
         User builder = new User.Builder()
-                .setId("")
+                .setId("id")
                 .setExternalId("")
-                .setNickName("")
+                .setNickName("nickname")
                 .build()
+				
+		then: "the return should be an User so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.id == "id"
+		builder.nickName == "nickname"
     }
 
     def "group should be able to build bottom to top"() {
-        given:
+        when: "an Group object is built with operation first"
         Group builder = new Group.Builder()
-                .setId("")
-                .setExternalId("")
+                .setId("id")
+                .setExternalId("external")
                 .setMembers(null)
                 .build()
+				
+		then: "the return should be an Group so no cast exception should be thrown"
+		notThrown(GroovyCastException)
+
+		and: "all values should be set correctly"
+		builder.id == "id"
+		builder.externalId == "external"
     }
 
 
